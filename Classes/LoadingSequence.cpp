0e866f17fb325c2bb16a9464bd6db7a60ea90e5d
ClearRiverFlow <ClearRiverFlowGaming@gmail.com>
2017-01-08 20:38:11 +0900

    Add comments to loading sequence.cpp

--- Classes/LoadingSequence.cpp
+++ Classes/LoadingSequence.cpp
@@ -23,45 +23,55 @@ bool LoadingSequence::init()
         return false;
     }
     
-    CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("Animation/OpeningHand/Sound/OpeningHand_SoundOnly.wav");
-    CocosDenshion::SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(1.0);
+    //====================== Preload Music =========================================//
     
-    //setAnimation();
+    //preloading opening hand animation background music
+    CocosDenshion::SimpleAudioEngine::getInstance()->preloadBackgroundMusic("Animation/OpeningHand/Sound/OpeningHand_SoundOnly.wav");
+    
+    //====================== Opening Hand Animation ================================//
+    
+    //set device size variables
     Size visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
-    Sprite *OpeningHand_0 = Sprite::create("Animation/OpeningHand/Image/OpeningHand_1To79_(1).png");
+    // setting animation
+    Sprite *OpeningHand_0 = Sprite::create("Animation/OpeningHand/Image/OpeningHand_1To79_(1).png"); // first appearence
     OpeningHand_0 -> setPosition( visibleSize.width/2 , visibleSize.height/2 + origin.y );
     OpeningHand_0 -> setScale( visibleSize.width / OpeningHand_0->getContentSize().width,
                               visibleSize.height / OpeningHand_0->getContentSize().height );
-    this->addChild(OpeningHand_0);
+    this->addChild(OpeningHand_0); // add child to layer
     
+    //create animation node
     auto animation = Animation::create();
-    float TotalDisplayLength = 3.78;
-    int MaxFrameSize = 79;
+    
+    // setting required variables for animation
+    float TotalDisplayLength = 3.78; // length of avi file
+    int MaxFrameSize = 79; // target frame size
     float DisplayTimePerFrame = TotalDisplayLength/MaxFrameSize;
     animation -> setDelayPerUnit(DisplayTimePerFrame);
     
+    // initiate animation
     for (int i=0; i<MaxFrameSize; i++)
     {
         animation -> addSpriteFrameWithFile(
                     StringUtils::format("Animation/OpeningHand/Image/OpeningHand_1To79_(%d).png", i+1)
                                             );
     }
-    
     auto animate = Animate::create(animation);
     int AnimationRepeatTimes = 1;
     auto action = Repeat::create(animate, AnimationRepeatTimes);
     OpeningHand_0 -> runAction(action);
     
-    
+    //setting volume for opening hand animation background music
+    CocosDenshion::SimpleAudioEngine::getInstance()->setBackgroundMusicVolume(1.0);
+    // play background music
     CocosDenshion::SimpleAudioEngine::getInstance()->playBackgroundMusic("Animation/OpeningHand/Sound/OpeningHand_SoundOnly.wav");
     
-    
+    // at the end of animation transit scene to menuscene
     this->scheduleOnce(schedule_selector(LoadingSequence::GoToMenuScene), DisplayTimePerFrame*MaxFrameSize*AnimationRepeatTimes );
     
     return true;
-}
+} // end of Loading squence init()
 
 //Replace Scene with Menuscene
 void LoadingSequence::GoToMenuScene(float dt)
