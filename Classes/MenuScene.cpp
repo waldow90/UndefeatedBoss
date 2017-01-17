f3486bc43d0b7f1abacff55fdaceaf9e2d0a1333
ClearRiverFlow <ClearRiverFlowGaming@gmail.com>
2017-01-17 20:52:00 +0900

    Creating MenuScene Menus using Sprite Image

--- Classes/MenuScene.cpp
+++ Classes/MenuScene.cpp
@@ -33,15 +33,49 @@ bool MenuScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
-    auto menu_item_1 = MenuItemFont::create("Play", CC_CALLBACK_1( MenuScene::Play, this ) );
-    auto menu_item_2 = MenuItemFont::create("Settings", CC_CALLBACK_1( MenuScene::Settings, this ) );
+    auto menu_item_play = MenuItemImage::create("Animation/MenuScene/PlayButton/PlayButton_(1).png"
+                                                , "Animation/MenuScene/PlayButton/PlayButton_(30).png"
+                                                , CC_CALLBACK_1( MenuScene::Settings, this ));
     
-    menu_item_1 -> setPosition( visibleSize.width/2, visibleSize.height/3 * 2 );
-    menu_item_2 -> setPosition( visibleSize.width/2, visibleSize.height/3 * 1 );
+
+    auto menu_item_setting = MenuItemImage::create("Animation/MenuScene/SettingButton/SettingButton_1To30_(1).png"
+                                                , "Animation/MenuScene/SettingButton/SettingButton_1To30_(30).png"
+                                                , CC_CALLBACK_1( MenuScene::Play, this ));
     
-    auto *menu = Menu::create(menu_item_1,menu_item_2,NULL);
+    menu_item_play -> setPosition( visibleSize.width/2, visibleSize.height/3 * 2 );
+    menu_item_play -> setScale( visibleSize.width/5 / menu_item_play->getContentSize().width
+                               , visibleSize.height/4 / menu_item_play->getContentSize().height );
+    
+    menu_item_setting -> setPosition( visibleSize.width/2, visibleSize.height/3 * 1 );
+    menu_item_setting -> setScale( visibleSize.width/5 / menu_item_play->getContentSize().width
+                               , visibleSize.height/4 / menu_item_play->getContentSize().height );
+    
+    auto *menu = Menu::create(menu_item_play,menu_item_setting,NULL);
     menu -> setPosition( Point(0,0) );
+    
+    /*
+    //create animation node
+    auto animation = Animation::create();
+    
+    // setting required variables for animation
+    float TotalDisplayLength = 1.25; // length of avi file
+    int MaxFrameSize = 30; // target frame size
+    float DisplayTimePerFrame = TotalDisplayLength/MaxFrameSize;
+    animation -> setDelayPerUnit(DisplayTimePerFrame);
+    
+    // initiate animation
+    for (int i=0; i<MaxFrameSize; i++)
+    {
+        animation -> addSpriteFrameWithFile(
+                                            StringUtils::format("Animation/MenuScene/PlayButton/PlayButton_(%d).png", i+1)
+                                            );
+    }
+    auto animate = Animate::create(animation);
+    auto action = RepeatForever::create(animate);
+    menu_item_play -> runAction(action);
+    */
     this -> addChild(menu);
+    
     
     return true;
 }
